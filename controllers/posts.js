ae49a51cf5821d2d0d5aa9476d429c13f703b76a
Kevin Lindholm <kevin.a.lindholm@hotmail.com>
2022-10-19 01:31:03 +0300

    remove unused import

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,12 +1,10 @@
 const Reviews = require("../models/Reviews")
-const hangeulTest = require("../models/HangeulTest");
 const Results = require("../models/Result");
 
 module.exports = {
   getProfile: async (req, res) => {
     try {
       const review = await Reviews.find();
-      const hangeul = await hangeulTest.find()
       console.log(req.user.id)
       const results = await Results.find({user: req.user.id})
       res.render("profile.ejs", { results, review, user: req.user });
