4fa21c2e582c4c989fc4a232c57e96c960a491f1
Kevin Lindholm <kevin.a.lindholm@hotmail.com>
2022-10-19 00:25:18 +0300

    change script for start

--- routes/hangeul.js
+++ routes/hangeul.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const hangeulController = require("../controllers/hangeulTest");
+const hangeulController = require("../controllers/hangeulTests");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 const upload = require("../middleware/multer");
 
